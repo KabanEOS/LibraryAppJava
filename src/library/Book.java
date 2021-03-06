ca0efed4321f284b2bbb20ba9fd5ee43d78a638f
Michał Madejski <49588850+KabanEOS@users.noreply.github.com>
2021-03-06 17:40:46 +0100

    project and presentation done

--- src/library/Book.java
+++ src/library/Book.java
@@ -6,66 +6,64 @@ public class Book {
     private String title, author, userDNI;
     private  Genre genre;
 
-        public Book() {
+    public Book() {
+        isbn = 0;
+        title = null;
+        author = null;
+        genre = Genre.NONE;
+        userDNI = null;
+    }
 
-            isbn = 0;
-            title = null;
-            author = null;
-            userDNI = null;
-            genre = Genre.NONE;
+    public Book(long isbn, String title, String author, Enum genre, String userDNI){
+            this.isbn = isbn;
+            this.title = title;
+            this.author = author;
+            this.genre = (Genre) genre;
+            this.userDNI = userDNI;
         }
 
-    public long getIsbn() {
-        return isbn;
-    }
+        public long getIsbn() {
+            return isbn;
+        }
 
-    public void setIsbn(long isbn) {
-        this.isbn = isbn;
-    }
+        public void setIsbn(long isbn) {
+            this.isbn = isbn;
+        }
 
-    public String getTitle() {
-        return title;
-    }
+        public String getTitle() {
+            return title;
+        }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+        public void setTitle(String title) {
+            this.title = title;
+        }
 
-    public String getAuthor() {
-        return author;
-    }
+        public String getAuthor() {
+            return author;
+        }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+        public void setAuthor(String author) {
+            this.author = author;
+        }
 
-    public String getUserDNI() {
-        return userDNI;
-    }
+        public String getUserDNI() {
+            return userDNI;
+        }
 
-    public void setUserDNI(String userDNI) {
-        this.userDNI = userDNI;
-    }
+        public void setUserDNI(String userDNI) {
+            this.userDNI = userDNI;
+        }
 
-    public Genre getGenre() {
-        return genre;
-    }
+        public Genre getGenre() {
+            return genre;
+        }
 
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
+        public void setGenre(Genre genre) {
+            this.genre = genre;
+        }
 
-    public Book(long isbn, String title, String author, String userDNI, Genre genre){
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
-        this.userDNI = userDNI;
-        this.genre = genre;
+        @Override
+        public String toString() {
+            return title+","+isbn+","+author +","+ genre +","+userDNI+"\n";
+        }
     }
-
-    @Override
-    public String toString() {
-        return title+","+isbn+","+author+","+userDNI +","+ genre +"\n";
-    }
-}
-
